5d9b1e454a79f14493b1d03bce18562a435358ef
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-05-31 17:58:04 +0200

    change input

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { tokenizer } from './tokenizer'
 import { parser } from './parser'
 import { evaluateAST } from './evaluator'
 
-const input = '10 + 10 * 10'
+const input = '10 + 10 * 10 /2 /2/ 2'
 
 const tokens = tokenizer(input)
 const ast = parser(tokens)
